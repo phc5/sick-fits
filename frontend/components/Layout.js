bfb4df73bb1fe158376d809cc6e4cc11360461cf
Paul <paulhyunchong@gmail.com>
2021-02-15 22:37:22 -0800

    Adds header and nav

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Header />
       <p>layout</p>
       {children}
     </div>
