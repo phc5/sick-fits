e8561eeba808f6540a5ff38e7b093d1845a86139
Paul <paulhyunchong@gmail.com>
2021-02-16 22:47:51 -0800

    Sets up styled-components, nprogress, and initial styles

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,51 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const StyledLogo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    align-items: center;
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Sick fits</Link>
+        <StyledLogo>
+          <Link href="/">Sick fits</Link>
+        </StyledLogo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,12 +1,66 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #E1E1E1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #EDEDED;
+    --maxWidth: 1000px;
+    --boxShadow: 0 12px 24px 0 rgba(0,0,0.09);
+    box-sizing: border-box;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1.5rem;
+    line-height: 2;
+    margin: 0;
+    padding: 0;
+  }
+
+  a{
+    text-decoration: none;
+    color: var(--black);
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+
+  button {
+    font-family: ---apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+`;
+
+const StyledInner = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      <p>layout</p>
-      {children}
+      <StyledInner>{children}</StyledInner>
     </div>
   );
 }
