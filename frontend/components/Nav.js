4e090c41c8225acffdb2780ed429c5c358a59ed3
Paul <paulhyunchong@gmail.com>
2021-02-18 20:42:44 -0800

    Fixes nav and header styling

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <Link href="/products">Products</Link>
       <Link href="/sell">Sell</Link>
       <Link href="/orders">Oders</Link>
       <Link href="/account">Account</Link>
-    </nav>
+    </NavStyles>
   );
 }
