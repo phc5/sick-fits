4d567b87bd17793d25f1e7e4dbbf0946e2f80628
Paul <paulhyunchong@gmail.com>
2021-02-18 21:34:31 -0800

    Adds Create product mutation

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,99 @@
+import { useMutation } from '@apollo/client';
+import { gql } from 'graphql-tag';
+import useForm from '../hooks/useForm';
+import ErrorMessage from './ErrorMessage';
+import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        photo: { create: { image: $image, altText: $name } }
+        status: "AVAILABLE"
+      }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm } = useForm({
+    name: '',
+    price: 0,
+    description: '',
+  });
+  const [
+    createProduct,
+    { loading, error },
+  ] = useMutation(CREATE_PRODUCT_MUTATION, { variables: inputs });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await createProduct();
+    clearForm();
+  }
+
+  if (error) return <ErrorMessage />;
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Add Product</button>
+      </fieldset>
+    </Form>
+  );
+}
