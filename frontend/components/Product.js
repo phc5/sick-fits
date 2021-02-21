f18beaaebd1a09192378803e0b292a3c94bb30f4
Paul <paulhyunchong@gmail.com>
2021-02-20 17:53:55 -0800

    Adds delete product functionality with cache eviction

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -3,6 +3,7 @@ import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
 
 // <Product/> is used in <Products/>
 export default function Product({ product }) {
@@ -28,6 +29,7 @@ export default function Product({ product }) {
         >
           Edit
         </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </ItemStyles>
   );
