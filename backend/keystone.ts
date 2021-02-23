e9a98cf3bc400de34aeda5ce9c8d6fde86bafe81
Paul <paulhyunchong@gmail.com>
2021-02-22 19:28:36 -0800

    Creates Cart Item in keystone backend

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 
@@ -55,6 +56,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     ui: {
       isAccessAllowed: ({ session }) => !!session?.data,
